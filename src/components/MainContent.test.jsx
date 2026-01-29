cd6bb9af83e5a86640b321aa222798c3eb34f67f
iamsreekanthrageni <ragenisreekanth64@gmail.com>
2026-01-29 11:45:40 +0530

    added comments [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 // My tests!
-// Add yet another comment
+// Add yet another comments
 
 describe('MainContent', () => {
   it('should render a button', () => {
